618d923c20ccf48e5672675f75659fdc2378442e
David Huynh <dh99.66.99@gmail.com>
2019-08-13 14:24:54 -0700

    add product page, add product type, add product categories

--- themes/inhabitent/header.php
+++ themes/inhabitent/header.php
@@ -21,6 +21,7 @@
 			<a class="skip-link screen-reader-text" href="#content"><?php echo esc_html( 'Skip to content' ); ?></a>
 
 			<header id="masthead" class="site-header" role="banner">
+			<div class="site-branding-container">
 				<div class="site-branding">
 				<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
 						<img src="<?php echo get_template_directory_uri() . '/images/logos/inhabitent-logo-tent.svg'; ?>" class="logo" alt="inhabitent logo tent green " />
@@ -40,6 +41,7 @@
 					</ul> -->
 				
 				</nav><!-- #site-navigation -->
+			</div>
 			</header><!-- #masthead -->
 
 			<div id="content" class="site-content">
